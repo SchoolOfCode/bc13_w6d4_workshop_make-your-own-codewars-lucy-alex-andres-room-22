875b6379a01dc57c13c9d7f4bc44c8a145ac88ba
L-McHugh <l.mchugh1@outlook.com>
2022-11-03 15:33:40 +0000

    seperate test names

--- main.test.js
+++ main.test.js
@@ -5,14 +5,29 @@ import {
 } from "./main.js";
 
 //👉 Write your tests below here:
-test ("Testing if the third digit is found", function () { expect (hundredDigit(3874)).toBe(8);
-    expect (hundredDigit(9574321)).toBe(3);
-    expect (hundredDigit(337443560)).toBe(5);
-    expect (hundredDigit(33760835451)).toBe(4);
-    expect (hundredDigit(9574821)).toBe(8);
-    expect (hundredDigit(837443650)).toBe(6);
-    expect (hundredDigit(33764835051)).toBe(0);
+test ("Testing the answer should be 8", function () { expect (hundredDigit(3874)).toBe(8);
 });
 
+test ("Testing the answer should be 3", function () { 
+    expect (hundredDigit(9574321)).toBe(3);
+});
 
-    
+test ("Testing the answer should be 5", function () { 
+    expect (hundredDigit(337443560)).toBe(5);
+});
+
+test ("Testing the answer should be 4", function () { 
+    expect (hundredDigit(33760835451)).toBe(4);
+});
+
+test ("Testing the answer should be 8", function () { 
+    expect (hundredDigit(9574821)).toBe(8);
+});
+
+test ("Testing the answer should be 6 ", function () { 
+    expect (hundredDigit(837443650)).toBe(6);
+});
+
+test ("Testing the answer should be 0", function () { 
+    expect (hundredDigit(33764835051)).toBe(0);
+});
