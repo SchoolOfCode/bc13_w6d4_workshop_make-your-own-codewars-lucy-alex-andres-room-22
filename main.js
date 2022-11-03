65a6905008c97857f68173aa394ba3245b73025b
L-McHugh <l.mchugh1@outlook.com>
2022-11-03 15:05:37 +0000

    Kata and tests created

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 /* 
 👉 Write your kata here!
 */
+// Return the third digit (the hundreds digit) of a non-negative number.
+
+// Example(Input => Output):
+// 352310 => 3
+// 45903234 => 2
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function hundredDigit(n) {
+    //code here
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,18 @@
+import { test, expect } from "@jest/globals";
+
+import {
+    hundredDigit,
+} from "./main.js";
+
 //👉 Write your tests below here:
+test ("Testing if the third digit is found", function () { expect (hundredDigit(3874)).toBe(8);
+    expect (hundredDigit(9574321)).toBe(3);
+    expect (hundredDigit(337443560)).toBe(5);
+    expect (hundredDigit(33760835451)).toBe(4);
+    expect (hundredDigit(9574821)).toBe(8);
+    expect (hundredDigit(837443650)).toBe(6);
+    expect (hundredDigit(33764835051)).toBe(0);
+});
+
+
+    
